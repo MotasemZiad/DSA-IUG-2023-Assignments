34ebb4d0f6140849e306e67b781031826e25a6d8
MotasemZiad <miziad2000@gmail.com>
2023-01-15 17:38:23 +0200

    apply the array deque

--- src/Assignment_1/ArrayDeque.java
+++ src/Assignment_1/ArrayDeque.java
@@ -1,5 +1,66 @@
 package Assignment_1;
 
 public class ArrayDeque {
+    private int front;
+    private int rear;
+    private int size;
+    private int[] array;
+
+    public ArrayDeque(int capacity) {
+        array = new int[capacity];
+        front = 0;
+        rear = 0;
+        size = 0;
+    }
+
+    public boolean isFull() {
+        return size == array.length;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public void insertFront(int element) {
+        if (isFull()) {
+            System.out.println("Deque is full.");
+            return;
+        }
+        front = (front - 1 + array.length) % array.length;
+        array[front] = element;
+        size++;
+    }
+
+    public void insertRear(int element) {
+        if (isFull()) {
+            System.out.println("Deque is full.");
+            return;
+        }
+        array[rear] = element;
+        rear = (rear + 1) % array.length;
+        size++;
+    }
+
+    public int deleteFront() {
+        if (isEmpty()) {
+            System.out.println("Deque is empty.");
+            return -1;
+        }
+        int element = array[front];
+        front = (front + 1) % array.length;
+        size--;
+        return element;
+    }
+
+    public int deleteRear() {
+        if (isEmpty()) {
+            System.out.println("Deque is empty.");
+            return -1;
+        }
+        rear = (rear - 1 + array.length) % array.length;
+        int element = array[rear];
+        size--;
+        return element;
+    }
 
 }
